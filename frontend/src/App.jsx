bbce47667a7f7511ab54a581caf7deb45c93c799
Tushar Suryawanshi <tushar.823737@gmail.com>
2025-04-18 01:53:18 +0530

    initialize React frontend with Vite and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>TODO APP</h1>
+      <button className="bg-green-500 hover:bg-green-600 font-bold p-8 text-2xl focus:outline-none">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
